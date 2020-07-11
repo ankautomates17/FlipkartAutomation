30bc1713cafe64fe9d27dc6f49b74fafe3dae6d5
Your Name <you@example.com>
2020-07-11 18:03:57 +0530

    Flipkart-LoginPage Test done

---
+++ src/main/java/com/flipkart/qa/pages/LoginPage.java
@@ -0,0 +1,48 @@
+package com.flipkart.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.flipkart.qa.base.TestBase;
+
+public class LoginPage extends TestBase {
+	
+	//Page Factory - Object Repository (OR):
+	
+	@FindBy(xpath = "//input[@class=\"_2zrpKA _1dBPDZ\"]")
+	WebElement email;
+	
+	@FindBy(xpath = "//input[@type=\"password\"]")
+	WebElement password;
+	
+	@FindBy(xpath = "//button[@class=\"_2AkmmA _1LctnI _7UHT_c\"]")
+	WebElement loginBtn;
+	
+	@FindBy(xpath = "//img[contains(@title,'Flipkart')]")
+	WebElement flipkartLogo;
+	
+	
+	//Initializing the Page Objects
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Actions:
+	public String validateLoginPageTitle() {
+		return driver.getTitle();
+	}
+	
+	public boolean validateFlipkartLogo() {
+		return flipkartLogo.isDisplayed();
+	}
+	
+	public HomePage login(String id, String pwd) {
+	    email.sendKeys(id);
+		password.sendKeys(pwd);
+		loginBtn.click();
+		
+		return new HomePage(); //return type is HomePage object once we click on login button
+	}
+		
+}
---
+++ src/test/java/com/flipkart/qa/tests/LoginPageTest.java
@@ -0,0 +1,50 @@
+package com.flipkart.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.flipkart.qa.base.TestBase;
+import com.flipkart.qa.pages.HomePage;
+import com.flipkart.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase{
+	
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginPageTest() {
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp() {
+		initialization();
+		loginPage = new LoginPage();
+	}
+	
+	@Test(priority=1)
+	public void loginPageTitleTest() {
+		String title = loginPage.validateLoginPageTitle();
+		Assert.assertEquals(title, "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!");
+	}
+	
+	@Test(priority=2)
+	public void flipkartLogoTest() {
+		boolean flag = loginPage.validateFlipkartLogo();
+		Assert.assertTrue(flag);
+	}
+	
+	@Test(priority=3)
+	public void loginTest() {
+		homePage = loginPage.login(prop.getProperty("email"), prop.getProperty("password")); //login method is returning homePage class object
+	}
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
